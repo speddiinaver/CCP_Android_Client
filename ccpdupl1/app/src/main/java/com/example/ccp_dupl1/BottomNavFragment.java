ad7bd2e8f024f7f02ad6b8d72689e5d378244028
DESKTOP-3F2KJ7I\Administrator <speddiinaver@gmail.com>
2021-09-13 22:22:16 +0900

    Error occurred./.

---
+++ ccpdupl1/app/src/main/java/com/example/ccp_dupl1/BottomNavFragment.java
@@ -0,0 +1,6 @@
+package com.example.ccp_dupl1;
+
+import android.app.Activity;
+
+public class BottomNavFragment extends Activity {
+}
---
+++ ccpdupl1/app/src/main/java/com/example/ccp_dupl1/BottomNavViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.ccp_dupl1
+
+import androidx.lifecycle.ViewModel
+
+class BottomNavViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
