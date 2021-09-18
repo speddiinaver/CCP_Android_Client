b94363d368e782a3f815a90f599a1cb83dd9cc2b
DESKTOP-LG38FBH\BrawnyClover <sonbill971@gmail.com>
2021-09-18 22:34:08 +0900

    create activity java and xml files

---
+++ ccpdupl1/app/src/main/java/com/example/ccp_dupl1/TrendsActivity.java
@@ -0,0 +1,14 @@
+package com.example.ccp_dupl1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trends);
+    }
+}
