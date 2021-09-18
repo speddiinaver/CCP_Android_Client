8fd3f17f35585054615f282ce3c9049db3107171
DESKTOP-LG38FBH\BrawnyClover <sonbill971@gmail.com>
2021-09-18 23:29:28 +0900

    SplashActivity 코드 완성

--- ccpdupl1/app/src/main/java/com/example/ccp_dupl1/SplashActivity.java
+++ ccpdupl1/app/src/main/java/com/example/ccp_dupl1/SplashActivity.java
@@ -2,7 +2,10 @@ package com.example.ccp_dupl1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        //로딩화면 시작.
+        Loadingstart();
+    }
+    private void Loadingstart(){
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
     }
 }
